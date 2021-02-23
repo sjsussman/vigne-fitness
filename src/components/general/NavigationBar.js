106920657b0e88d688b8cd70b8923fd2c5498818
Steven Sussman <sjsussman@gmail.com>
2021-02-23 16:44:51 -0500

    added base navbar styling

--- src/components/general/NavigationBar.js
+++ src/components/general/NavigationBar.js
@@ -1,29 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import NavigationLinks from "./NavigationLinks";
 import Logo from "../../images/Logo.jpg";
+import "../../styles/styles.scss";
 
 const NavigationBar = () => {
-  const [navbarOpen, setNavbarOpen] = useState(false);
-
   return (
-    <nav classname="navigation">
-      <img src={Logo} />
-      <div
-        className="toggle"
-        navbarOpen={navbarOpen}
-        onClick={() => setNavbarOpen(!navbarOpen)}
-      >
-        {navbarOpen ? <div className="hamburger" open /> : <div />}
+    <nav className="navigation">
+      <img id="logo" src={Logo} alt="vigne logo" />
+      <div className="navbox">
+        <NavigationLinks />
       </div>
-      {navbarOpen ? (
-        <div className="navbox">
-          <NavigationLinks />
-        </div>
-      ) : (
-        <div classname="navbox" open>
-          <NavigationLinks />
-        </div>
-      )}
     </nav>
   );
 };
